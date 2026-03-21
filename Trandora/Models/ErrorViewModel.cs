e10afac165703dd15aba4f511351411c4980d3da
parthasarathimanikandeeswaran-afk <parthasarathimanikandeeswaran@gmail.com>
2026-03-21 20:01:04 +0530

    Delete Trandora directory

--- Trandora/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Trandora.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
